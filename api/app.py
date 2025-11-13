531e3d16a5913ec199e967eba3966f3afdebd76a
duarteeigor <duarteigor562@gmail.com>
2025-11-13 18:44:17 -0300

    feat: initializing server

---
+++ api/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from api.text_processing import  processText
+from api.classifier import classify_email
+from api.generated_response import gerar_resposta_api
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/analisar', methods=['POST'])
+def analisar():
+    email_text = request.form['email']
+    if not email_text.strip():
+        return "Nenhuma mensagem enviada"
+    
+    formated = processText(email_text)
+    category = classify_email(formated)
+    response = gerar_resposta_api(formated)
+
+    print("Texto recebido: ", formated, category)
+    return f"Categoria: {category}\nResposta sugerida: {response}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+def handler(request, *args, **kwargs):
+    return app(request.environ, request.start_response)
+
+
+
